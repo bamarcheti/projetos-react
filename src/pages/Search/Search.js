74fa693ec50e7b5abdad57fb02c8bff46bf2cda2
Bamarcheti <bmarchetifiorin@gmail.com>
2022-11-13 17:38:01 -0400

    criando arquivo search

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,8 +1,61 @@
-function Search() {
+import React, { useState } from 'react';
+import '../App.css';
+
+function Search(props) {
+  const [cidade, setCidade] = useState("");
+
+  function searchInput(e) {
+    
+    e.preventDefault();
+    let currentValue = document.querySelector('input[name=searchInput]').value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${currentValue}&appid=4d8fb5b93d4af21d66a2948710284366&units=metric`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const { main, name, sys, weather } = data;
+
+        if (sys !== undefined) {
+          if (weather !== undefined) {
+            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+            setCidade(`
+            <div>
+              <p>${main}</p>
+              <p>${sys}</p>
+              <p>${name}</p>
+              <p>${weather[0]['description']}</p>
+              <img src="${icon}" />
+            </div>      
+          `)
+          }
+        } else {
+          setCidade("");
+        }
+      })
+  }
+
   return (
-    <div>
-      OLA
+    <div className="searchWraper">
+      <div className="search">
+        <h2>Digite o nome da cidade:</h2>
+        <form onSubmit={(e) => searchInput(e)}>
+          <input
+            placeholder={props.placeholder}
+            type="text"
+            name="seachInput"
+          />
+          <input
+            type="submit"
+            value="Pesquisar por cidade!"
+          />
+        </form>
+      </div>
+      {
+        (cidade !== "") ?
+        <div dangerouslySetInnerHTML={{__html: cidade}} /> : <div>Pesquise por algo acima...</div>
+      }
     </div>
+
   )
 }
 
